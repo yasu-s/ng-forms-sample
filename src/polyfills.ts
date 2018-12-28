988f771014f3e2535a6a11512cee24d15dccdd8f
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-12-28 19:55:16 +0900

    yarn移植

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,6 +33,15 @@ import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
+import 'core-js/es7/symbol';
+import 'core-js/es7/object';
+import 'core-js/es7/math';
+import 'core-js/es7/string';
+import 'core-js/es7/array';
+import 'core-js/es7/map';
+import 'core-js/es7/weak-map';
+import 'core-js/es7/weak-set';
+import 'core-js/es7/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
